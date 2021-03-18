f85e666eb78c041da1019b81187b1c07c15d71ca
camilo quinones <u5500360@unimilitar.edu.co>
2021-03-18 11:12:36 +0800

    Create train.py , extract.py and rule.py

--- MLGame-master/games/arkanoid/log/pickle.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-filename = 'manual_EASY_3_2021-03-04_16-18-41'
-outfile = open(filename,'wb')
-pickle.dump()
--- MLGame-master/games/arkanoid/ml/rule.py
+++ MLGame-master/games/arkanoid/ml/rule.py
@@ -11,9 +11,10 @@ class MLPlay:
         self.ball_pos = [93,93]
         self.ball_served = False
 
-    def update(self, scene_info):
+    def update(self, scene_info): 
+    
         ballPos = self.ball_pos
-        print (scene_info["ball"])
+        
         """
         Generate the command according to the received `scene_info`.
         """
@@ -23,9 +24,16 @@ class MLPlay:
             scene_info["status"] == "GAME_PASS"):
             return "RESET"
 
+        # if not self.ball_served:
+        #     self.ball_served = True
+        #     command = "SERVE_TO_RIGHT"    
         if not self.ball_served:
-            self.ball_served = True
-            command = "SERVE_TO_LEFT"    
+            if scene_info["platform"][0] > 100:
+                self.ball_served = True
+                command = "SERVE_TO_LEFT"
+            else:
+                command = "MOVE_RIGHT"
+            return command
 
         if (scene_info["ball"][1] - ballPos[1] != 0 and ballPos[1] > 150):
             xVal = ballPos[0] + ((400 - ballPos[1]) * ((scene_info["ball"][0] - ballPos[0]) / (scene_info["ball"][1] - ballPos[1])))
@@ -48,44 +56,9 @@ class MLPlay:
         if scene_info["platform"][0] < xVal - 20:
             command = "MOVE_RIGHT"
         
-        print(command)
+
         return command 
-        # if scene_info["frame"]==0:
-        #     current_ball_x = scene_info["ball"][0]
-        #     current_ball_y = scene_info["ball"][1]
-        #     des_x = 100
         
-        # else:
-        #     last_ball_x = current_ball_x
-        #     last_ball_y = current_ball_y
-        #     current_ball_x=scene_info["ball"][0]
-        #     current_ball_y=scene_info["ball"][1]
-
-        #     if current_ball_y>last_ball_y:
-        #         if current_ball_x>last_ball_x:
-        #            des_x=(400-current_ball_y)+current_ball_x	
-        #         else:
-        #            des_x=current_ball_x-(400-current_ball_y)
-        #     if current_ball_y<last_ball_y:
-        #         des_x=100
-
-        # while des_x>200 or des_x<0:
-            
-        #     if des_x>200:
-        #         des_x=(200-(des_x-200))
-        #     else:
-        #         des_x=-des_x
-					
-		
-		
-		# ## 3.4. Send the instruction for this frame to the game process
-        # if des_x<scene_info["platform"][0]+25:
-        #     command = "MOVE_LEFT"
-        # elif des_x>scene_info["platform"][0]+25:
-        #     command = "MOVE_RIGHT"
-        # else:
-        #     command = "NONE"	            
-
     def reset(self):
         """
         Reset the status
